4f67462a0266af48182180a43ce10c3b522a2ceb
Rory Molyneux <rory@pitchdark.co.za>
2018-04-01 12:09:25 +0200

    Added tracking for single pages regions. Formatting still busted, will fix later

--- pd-category-tracker.php
+++ pd-category-tracker.php
@@ -18,11 +18,12 @@ add_action('wp_footer', 'send_event');
 
 function send_event() {
     
+    $analytics_obj = 'wppas_ga';
+    
     if(is_archive()) {
-        
+        // Get the name of the archive
         $archive_type = get_the_archive_title();
-        $analytics_obj = 'wppas_ga';
-                
+                        
         echo '<script>
         function sendAnalyticsEvent() {
         
@@ -49,4 +50,45 @@ sendAnalyticsEvent();
         
     }
     
+    if(is_single()) {
+        // Set the parent category
+        $parent_cat = 16;
+        
+        // Get all child categories of a parent
+        $categories = get_categories(array('child_of' => $parent_cat));
+        
+        echo '<script>
+        function sendAnalyticsEvent() {
+        
+    if(typeof ' . $analytics_obj . ' === "undefined") {
+        setTimeout(sendAnalyticsEvent(), 1000);
+    } else {
+    
+        ';
+        foreach($categories as $cat) {
+                            
+                echo "\r\n" . 'var obj = {
+            hitType: "event",
+            eventCategory: "wp category statistics",
+            eventAction: "impressions",
+            eventLabel: "' . $cat->name . '"
+        }
+        // console.log(obj)
+            
+        ' . $analytics_obj . '("send", obj);';
+                
+        }
+        // Close off JS function
+        echo "\r\n}\r\n}";
+        
+        // Do function
+        echo "\r\nsendAnalyticsEvent();
+</script>";
+        
+        
+    }
+    
+
+        
 }
+    
